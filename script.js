7f8526b626316ba905b29b7c13663f16a1c5c76d
Prakriti Pokharel <iprakritip@gmail.com>
2023-05-04 11:36:22 +0545

    update progress bar while video plays

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer")
 const progress = player.querySelector(".progress")
-const progressBar = player.querySelectorAll(".progress__filled")
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector(".toggle")
 const skipButtons = player.querySelectorAll("[data-skip]")
 const ranges = player.querySelectorAll('.player__slider')
@@ -26,28 +26,34 @@ function updateButton() {
     toggle.innerHTML = icon
 }
 
-function skip(){  
+function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function hnadleRangeUpdate(){
-    video[this.name]=this.value
-    // console.log(this.value);
-    // console.log(this.name);
+function handleRangeUpdate() {
+    video[this.name] = this.value
 }
+
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+    
+  }
 
 // Hook up event listeners
 
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
+video.addEventListener('timeupdate', handleProgress )
 
 toggle.addEventListener('click', togglePlay)
 
-skipButtons.forEach(button=>[
+skipButtons.forEach(button => [
     button.addEventListener('click', skip)
 ])
-ranges.forEach(range=>{
-    range.addEventListener("change", hnadleRangeUpdate)
-    range.addEventListener("mousemove", hnadleRangeUpdate)
+ranges.forEach(range => {
+    range.addEventListener("change", handleRangeUpdate)
+    range.addEventListener("mousemove", handleRangeUpdate)
 })
